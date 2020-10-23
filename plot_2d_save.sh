cf1e6a3315e938bcdf7365478830386ae7947466
Sumeet Khanna <43139805+sumeet92k@users.noreply.github.com>
2020-10-23 17:33:21 +0530

    Update plot_2d_save.sh
    strips the input filename extension, and adds the output filename extension

--- plot_2d_save.sh
+++ plot_2d_save.sh
@@ -4,6 +4,10 @@ fname=$1
 field=$2
 echo $1
 echo $2
+suffix=".dat"
+fname_out=${fname%"$suffix"}.png
+echo $fname_out
+
 gnuplot -persist <<PLOT
 
 #set term wxt
@@ -11,10 +15,11 @@ set term png
 set pm3d
 unset surface
 set view map
-set xrange [100:2000]
+#set xrange [0:500]
 # set view equal xy
-#set size ratio -1
-set output "$fname.png"
+set size ratio -1
+set output "$fname_out"
 # replot
 splot '$fname' us 1:2:$field 
 PLOT
+
